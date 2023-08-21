faaa62c3bb1d26a4a31712e7100c2155b96ae027
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-08-21 19:04:00 +0530

    new:basic ui of the landing page create

--- client/src/containers/LandingPage/Hero.jsx
+++ client/src/containers/LandingPage/Hero.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import users from "../../assets/users.png";
 
-const Hero = () => {
-  const [number, setNumber] = useState(10);
+const Hero = ({ img, number, title, isLeft }) => {
+  //   const [number, setNumber] = useState(10);
   return (
-    <section>
-      <div>
-        <div>
-          <h1>Number of Users</h1>
-          <p>{number}</p>
+    <section className="flex flex-col justify-center items-center pt-[6rem]">
+      <div
+        className={`flex ${
+          isLeft ? "lg:flex-row" : "lg:flex-row-reverse"
+        } md:flex-col-reverse gap-10 px-6 w-full items-center justify-between py-8`}
+      >
+        <div className="lg:w-[50%] w-[90%] flex flex-col justify-start items-center py-6">
+          <div className="flex flex-col justify-start items-start gap-5">
+            <h1 className="lg:text-4xl font-semibold md:text-3xl sm:text-2xl text-xl">
+              {title}
+            </h1>
+            <p className="lg:text-3xl font-semibold md:text-2xl sm:text-xl text-lg">
+              {number}
+            </p>
+          </div>
         </div>
-        <div>
-          <img src={users} alt="img/users" />
+        <div className="lg:w-[30%] w-[90%] flex flex-col justify-center items-center">
+          <img src={img} alt="img/users" />
         </div>
       </div>
     </section>
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Hero } from '../../containers/LandingPage'
+import React from "react";
+import { Hero } from "../../containers/LandingPage";
+import users from "../../assets/users.png";
+import admins from "../../assets/admins.png";
+import orders from "../../assets/orders.jpeg";
+import products from "../../assets/products.jpeg";
 
 const LandingPage = () => {
   return (
     <>
-      <Hero/>
+      <Hero img={users} number={10} title={"Number of Users"} isLeft={true} />
+      <Hero
+        img={admins}
+        number={10}
+        title={"Number of Admins"}
+        isLeft={false}
+      />
+      <Hero
+        img={products}
+        number={10}
+        title={"Number of Products Till Now"}
+        isLeft={true}
+      />
+      <Hero
+        img={orders}
+        number={10}
+        title={"Number of Orders Till Now"}
+        isLeft={false}
+      />
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
