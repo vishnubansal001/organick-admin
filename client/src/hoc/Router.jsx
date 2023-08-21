d91b4169a72e678d11f2d554667f3b0f90daeee6
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-08-21 17:27:46 +0530

    started the project

---
+++ client/src/hoc/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element></Route>
+    </Routes>
+  )
+}
+
+export default Router
