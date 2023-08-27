34786d1b1fabc80cba352f035b35d0131b9e20d2
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-08-27 18:41:47 +0530

    new:Added the landing page api for getting the number of admins users products news and orders

--- server/controllers/landing.js
+++ server/controllers/landing.js
@@ -1,9 +1,27 @@
 const Product = require("../models/Product");
 const User = require("../models/User");
 const Orders = require("../models/Orders");
+const News = require("../models/News");
 
 const landingData = async (req, res) => {
-    
+  try {
+    const productCount = await Product.countDocuments();
+    const newsCount = await News.countDocuments();
+    const orderCount = await Orders.countDocuments();
+    const userCount = await User.countDocuments();
+    const adminCount = await User.countDocuments({ isAdmin: true });
+
+    return res.json({
+      products: productCount,
+      news: newsCount,
+      orders: orderCount,
+      users: userCount,
+      admins: adminCount,
+    });
+  } catch (error) {
+    console.error("Error:", err);
+    return res.status(500).json({ error: "An error occurred" });
+  }
 };
 
 module.exports = { landingData };
